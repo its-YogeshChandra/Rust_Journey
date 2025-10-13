3b6ecfeb1233e252cc5781f46dfa97862b4bc23f
LG-Crusty <pandittheroyal@gmail.com>
2025-10-14 03:32:25 +0530

    created first programme

---
+++ firstProgramme/target/debug/deps/firstprogramme-8d0ec1f68e889fe2.d
@@ -0,0 +1,5 @@
+/home/crusty/Documents/Rust/firstProgramme/target/debug/deps/firstprogramme-8d0ec1f68e889fe2.d: src/main.rs
+
+/home/crusty/Documents/Rust/firstProgramme/target/debug/deps/firstprogramme-8d0ec1f68e889fe2: src/main.rs
+
+src/main.rs:
---
+++ firstProgramme/target/debug/firstprogramme.d
@@ -0,0 +1,1 @@
+/home/crusty/Documents/Rust/firstProgramme/target/debug/firstprogramme: /home/crusty/Documents/Rust/firstProgramme/src/main.rs
