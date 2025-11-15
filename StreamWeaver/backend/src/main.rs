a7c2bcb228ad4a2df2a11907955b3dd9cbec82ef
its-YogeshChandra <pandittheroyal@gmail.com>
2025-11-15 16:42:49 +0530

    basic single thread is working fine

--- StreamWeaver/backend/src/main.rs
+++ StreamWeaver/backend/src/main.rs
@@ -1,5 +1,7 @@
+use std::fs::File;
 use std::io::Read;
 use std::io::Write;
+// use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
 
@@ -17,15 +19,48 @@ fn main() {
     }
 }
 
-fn handle_connection(mut stream: TcpStream) {
+fn handle_connection(mut stream: TcpStream) -> String {
     //creating buffer
+    //Buffers in Rust: In Rust, a buffer is typically a block of memory used for temporary storage of data. Buffers are commonly used when reading or writing data to or from sources like files, network sockets, or memory
     let mut buffer = [0; 512];
 
     stream.read(&mut buffer).unwrap();
 
-    //creating a response
-    let response = "HTTP/1.1 200 OK \r\n\r\n";
+    let get = b"GET / HTTP/1.1\r\n";
 
-    stream.write(response.as_bytes()).unwrap();
-    stream.flush().unwrap();
+    if buffer.starts_with(get) {
+        println!("inside the function");
+        let mut file = File::open("hello.html").unwrap();
+
+        let mut contents = String::new();
+        match file.read_to_string(&mut contents) {
+            Ok(n) => {
+                println!("reading done : {}", n);
+            }
+            Err(e) => {
+                eprintln!("failed to read from file: {}", e);
+            }
+        }
+        //creating a response
+        let response = format!(
+            "HTTP/1.1 200 OK \r\nContent-Type: text/html\r\nContent-Length: {}\r\n\r\n{}",
+            contents.len(),
+            contents
+        );
+
+        stream.write(response.as_bytes()).unwrap();
+        stream.flush().unwrap();
+
+        let working = String::from("working fine");
+        working
+    } else {
+        println!("invalid route or request type");
+        let error = String::from("invalid route or request type");
+        error
+    }
 }
+
+//reeuest format
+//HTTP-Version Status-Code Reason-Phrase CRLF
+// headers CRLF
+// message-body
