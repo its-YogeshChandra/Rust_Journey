e81c9d2d7bf96758ddbf168a66241d7c46616b53
its-YogeshChandra <pandittheroyal@gmail.com>
2025-12-03 17:00:01 +0530

    saving commit

--- StreamWeaver/backend/src/controller/metadata.rs
+++ StreamWeaver/backend/src/controller/metadata.rs
@@ -21,4 +21,27 @@ pub fn meta_data_and_options(request: Request, stream: TcpStream) -> () {
     let ResponseBody { data }: ResponseBody<Value> = json_deserializer(&request.body_data);
 
     //match the data for;
+    let iterable_data = data.as_object();
+
+    //create a map of serde value to put the dat into
+    let obj = serde_json::Map::new();
+    let mut body_data = Value::Object(obj);
+
+    //match for the key
+    let keys = ["url"];
+    if let Some(data) = iterable_data {
+        //iterate the keys array
+        for key in keys {
+            //match the key with the obj
+            if !data.contains_key(key) {
+                //throw the error
+                let error = format!("{} not found ", key);
+                errorhandler(&stream, &error)
+            } else {
+                body_data[key] = data[key].clone()
+            }
+        }
+    }
+
+    //call the youtube api for the metadata available
 }
