2ec0a34b8e5ce13d7df34337160f96480b4d6cb0
its-YogeshChandra <pandittheroyal@gmail.com>
2025-12-05 00:54:41 +0530

    controller for getting metadata is working fine

--- StreamWeaver/backend/src/controller/metadata.rs
+++ StreamWeaver/backend/src/controller/metadata.rs
@@ -1,37 +1,31 @@
 //controller for getting meta data
 //bringig the reuest sturct  crate ;
-
 use crate::utils::Request;
 use crate::utils::Response;
 use crate::utils::handle_response;
-use dotenv::dotenv;
-use std::env;
 use std::net::TcpStream;
+use std::process::Stdio;
 // bringing  sirealization crate serde
 use crate::utils::errorhandler;
 use crate::utils::{ResponseBody, json_deserializer};
 use reqwest::Client;
 use serde::{Deserialize, Serialize};
 use serde_json::Value;
-use serde_json::map::Keys;
 use std::process::Command;
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
-use tokio::net::TcpListener;
-
-//#[derive(Serialize, Deserialize)]
-enum MetaInformation {
-    Url(Value),
-}
 
 #[tokio::main]
 pub async fn meta_data_and_options(request: Request, stream: TcpStream) -> () {
-    dotenv().ok();
     // get the data from the request
     let ResponseBody { data }: ResponseBody<Value> = json_deserializer(&request.body_data);
 
     //match the data for;
     let iterable_data = data.as_object();
 
+    //create value through the Data
+    // let mut body_data = Data {
+    //     url: String::from("marco")
+    // };
     //create a map of serde value to put the dat into
     let obj = serde_json::Map::new();
     let mut body_data = Value::Object(obj);
@@ -54,21 +48,19 @@ pub async fn meta_data_and_options(request: Request, stream: TcpStream) -> () {
     }
 
     //call the youtube api for the metadata available
-    let api_key = env::var("YOUTUBE_API_KEY").expect("YOUTUBE_API_KEY must be set");
-
-    //set the channel ID
-    // let url = format!(
-    //     "http://www.googleapis.com/youtube/v3/videos?part=snippet&id={}&key={}",
-    //     api_key,
-    //
-    // );
+    let video_url = body_data["url"].as_str().expect("expecting a string");
+    println!("video_url : {}", &video_url);
 
     //call the yt-dlp
-    let child = Command::new("yt-dlp")
+    let ytdlp_process = Command::new("yt-dlp")
         .arg("--list-formats")
-        .arg(url)
+        .arg(video_url)
+        .stdout(Stdio::piped())
         .output()
         .expect("failed to execute the process");
-    //
-    //
+
+    //fix the data to that string
+    let output = String::from_utf8_lossy(&ytdlp_process.stdout);
+
+    println!("output : {}", output);
 }
---
+++ StreamWeaver/backend/target/debug/deps/backend-44374a7aa2b19c0e.d
@@ -0,0 +1,16 @@
+/Users/test/Documents/Rust/StreamWeaver/backend/target/debug/deps/backend-44374a7aa2b19c0e.d: src/main.rs src/routes/mod.rs src/routes/routes.rs src/utils/mod.rs src/utils/errorhandler.rs src/utils/jsondeserializer.rs src/utils/requesthandler.rs src/utils/responsesystem.rs src/utils/routehandler.rs src/controller/mod.rs src/controller/metadata.rs src/controller/sendresponse.rs
+
+/Users/test/Documents/Rust/StreamWeaver/backend/target/debug/deps/backend-44374a7aa2b19c0e: src/main.rs src/routes/mod.rs src/routes/routes.rs src/utils/mod.rs src/utils/errorhandler.rs src/utils/jsondeserializer.rs src/utils/requesthandler.rs src/utils/responsesystem.rs src/utils/routehandler.rs src/controller/mod.rs src/controller/metadata.rs src/controller/sendresponse.rs
+
+src/main.rs:
+src/routes/mod.rs:
+src/routes/routes.rs:
+src/utils/mod.rs:
+src/utils/errorhandler.rs:
+src/utils/jsondeserializer.rs:
+src/utils/requesthandler.rs:
+src/utils/responsesystem.rs:
+src/utils/routehandler.rs:
+src/controller/mod.rs:
+src/controller/metadata.rs:
+src/controller/sendresponse.rs:
